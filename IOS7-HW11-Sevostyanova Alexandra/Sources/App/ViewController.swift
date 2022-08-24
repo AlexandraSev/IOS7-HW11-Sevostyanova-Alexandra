732c9ed355042feaca14fb6a2dcb3d0467e77bf8
Alexandra <AlexandraSevost@icloud.com>
2022-08-24 20:55:39 +0300

    Preparing for layout in code, creating a background image

--- IOS7-HW11-Sevostyanova Alexandra/Sources/App/ViewController.swift
+++ IOS7-HW11-Sevostyanova Alexandra/Sources/App/ViewController.swift
@@ -8,18 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // MARK: - Background picture
+    // MARK: - Outlets
     private lazy var imageView: UIImageView = {
         let image = UIImage(named: "background picture")
         let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
         return imageView
     }()
-
+    
+    
+    
+ //MARK: - Lifestyle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupHierarchy()
+        setupLayout()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+
+ // MARK: - Setup
+    private func setupHierarchy() {
+        view.addSubview(imageView)
     }
 
-
+    private func setupLayout() {
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 100).isActive = true
+        imageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: -100).isActive = true
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+    }
+    
+    
+// MARK: - Actions
+    
 }
 
